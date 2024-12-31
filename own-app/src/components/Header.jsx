d4065f70bb168b441e15fb85774a047dfc585e2c
WalterGrimes <bako.bota2009@gmail.com>
2024-12-31 19:06:22 +0600

    сегодня нг,просто добавлены модульные css и чуток деструктуризации

--- own-app/src/components/Header.jsx
+++ own-app/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import Reacts from "react";
+import s from './Header.module.css';
 
 const Header = () => {
      return (
-        <header className='header'>
+        <header className={s.header}>
         <img src='https://avatars.mds.yandex.net/i?id=0cabfd4d88785bb87a448bbf767de929ed77174e-10814666-images-thumbs&n=13' alt='header logo' />
       </header>
      )
--- own-app/src/components/Profile.jsx
+++ own-app/src/components/Profile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className='content'>
-      <div>
-        <img src='https://avatars.mds.yandex.net/get-mpic/4304254/img_id8368786378751231346.jpeg/orig' alt='content image' />
-      </div>
+    <div className={s.content}>
+      <img
+        src="https://avatars.mds.yandex.net/get-mpic/4304254/img_id8368786378751231346.jpeg/orig"
+        alt="content image"
+      />
       <div>
         ava + description
       </div>
@@ -14,9 +16,9 @@ const Profile = () => {
         <div>
           New post
         </div>
-        <div>
-          <div>post 1</div>
-          <div>post 2</div>
+        <div className={s.posts}>
+          <div className={s.item}>post 1</div>
+          <div className={s.item}>post 2</div>
         </div>
       </div>
     </div>
