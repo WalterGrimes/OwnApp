e524b61428bb7915f4b8443efc5ee9374cdf459b
WalterGrimes <bako.bota2009@gmail.com>
2025-01-01 21:19:01 +0600

    Router,BrowserRouter и так далее 19.

--- own-app/src/App.js
+++ own-app/src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile'
+import Profile from './components/Profile/Profile';
 import Whyus from './components/Whyus/Whyus';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      {/* <Profile/>  */}
-      <div className = 'app-wrapper-content'>
-      <Whyus />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/whyus' element={<Whyus />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
