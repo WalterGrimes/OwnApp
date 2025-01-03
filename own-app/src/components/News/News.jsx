86b4bb5829ecb5cdf00453ca69fa378c42e378d5
WalterGrimes <bako.bota2009@gmail.com>
2025-01-03 15:58:45 +0600

    Создание некого BLL и доступ к ним не напрямую,немного рефакторинга в целом это основное

--- own-app/src/components/News/News.jsx
+++ own-app/src/components/News/News.jsx
@@ -13,13 +13,23 @@ let path = '/news/' + props.id;
     );
 }
 
+
 const News = () => {
+
+    let NewsData =[
+        {id: 1, advantage : 'Protein Maxler' },
+        {id: 2, advantage : 'Mezomorf crazy'},
+        {id: 3, advantage : 'Creatine blue'},
+        {id: 4, advantage : 'Gainer Bucked up'},
+    ]
+
+
     return (
         <div>
             <div className={s.News}>
-                <div className={s.Newitem}>
-                    <NewsItem advantage='Protein Maxler' id='1' />
-                    <NewsItem advantage='Mezomorf crazy' id='2' />
+                <div className={s.Newitems}>
+                    <NewsItem advantage={NewsData[0].name} id={NewsData[0].id} />
+                    <NewsItem advantage={NewsData[1].name} id={NewsData[1].id} />
                     <NewsItem advantage='Creatine blue' id='3' />
                     <NewsItem advantage='Gainer Bucked up' id='4' />
                   
--- own-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ own-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,14 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+  // Данные постов
+  let postData = [
+    { rating: "Most popular", ratingStatus: "4.6 stars", comment: "I like this protein" },
+    { rating: "Best value", ratingStatus: "2.0 stars", comment: "Guys, that's not a protein(" },
+    { rating: "New release", ratingStatus: "5.0 stars", comment: "Amazing product!" },
+    { rating: "Budget option", ratingStatus: "3.5 stars", comment: "Good for the price." },
+  ];
+
   return (
     <div>
       <div>My items</div>
@@ -10,9 +18,14 @@ const MyPosts = () => {
         <button>Buy product</button>
       </div>
       <div className={s.posts}>
-        <Post rating="Most popular" ratingStatus=" 4.6 stars" comment="i like this protein"/>
-        <Post rating="Best value" ratingStatus=" 2.0 stars" comment="guys,thats not a protein("/>
-
+        {postData.map((post, index) => (
+          <Post 
+            key={index}
+            rating={post.rating} 
+            ratingStatus={post.ratingStatus} 
+            comment={post.comment} 
+          />
+        ))}
       </div>
     </div>
   );
