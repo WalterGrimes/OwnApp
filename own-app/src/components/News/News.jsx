8528a593b9ff1059810a3b2eb1d44c48a22a186c
WalterGrimes <bako.bota2009@gmail.com>
2025-01-03 17:05:53 +0600

    Жесткий мапинг и сокращение и улучшение кода используя map-метод массива

--- own-app/src/components/News/News.jsx
+++ own-app/src/components/News/News.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 // Подкомпонента для отображения преимущества
 const NewsItem = (props) => {
-let path = '/news/' + props.id;
+    let path = '/news/' + props.id;
 
     return (
         <div className={s.newproduct}>
@@ -13,26 +13,23 @@ let path = '/news/' + props.id;
     );
 }
 
-
 const News = () => {
-
-    let NewsData =[
-        {id: 1, advantage : 'Protein Maxler' },
-        {id: 2, advantage : 'Mezomorf crazy'},
-        {id: 3, advantage : 'Creatine blue'},
-        {id: 4, advantage : 'Gainer Bucked up'},
+    let NewsData = [
+        { id: 1, advantage: 'Protein Maxler' },
+        { id: 2, advantage: 'Mezomorf crazy' },
+        { id: 3, advantage: 'Creatine blue' },
+        { id: 4, advantage: 'Gainer Bucked up' },
     ]
 
+    // Используем правильное свойство "advantage"
+    let NewsElements = NewsData
+        .map(newP => <NewsItem advantage={newP.advantage} id={newP.id} />)
 
     return (
         <div>
             <div className={s.News}>
                 <div className={s.Newitems}>
-                    <NewsItem advantage={NewsData[0].name} id={NewsData[0].id} />
-                    <NewsItem advantage={NewsData[1].name} id={NewsData[1].id} />
-                    <NewsItem advantage='Creatine blue' id='3' />
-                    <NewsItem advantage='Gainer Bucked up' id='4' />
-                  
+                    {NewsElements}
                 </div>
             </div>
         </div>
